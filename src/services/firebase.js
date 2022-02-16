7615abe34dda29810e91310699988b90697c001f
Sebastian2504 <sebi.rassloff@live.com>
2022-02-16 19:31:01 +0100

    first commit

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,17 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
-// import firebaseConfig from "./firebaseConfig"
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCf16TuI3nuCCgTmNHqluva5-ETEzmOVyo",
-    authDomain: "movieapp-5f61c.firebaseapp.com",
-    databaseURL: "https://movieapp-5f61c-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "movieapp-5f61c",
-    storageBucket: "movieapp-5f61c.appspot.com",
-    messagingSenderId: "389550078713",
-    appId: "1:389550078713:web:236f43f61df65169aec028"
-  };
+import firebaseConfig from "./firebaseConfig"
 
 export const app = initializeApp(firebaseConfig);
 export const db = getDatabase();
